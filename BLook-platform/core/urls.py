51a290799833bf93370d22a469747c967e0a5c7c
Pal-C <acrodriguez4@up.edu.ph>
2025-05-01 19:17:51 +0800

    Added logout button in home (temp), initial css for auth screen

--- BLook-platform/core/urls.py
+++ BLook-platform/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("", views.home, name='home'),
     path("auth/", views.auth_screen, name="auth_screen"),
+    path('logout/', LogoutView.as_view(next_page='auth_screen'), name='logout'),
 ]
